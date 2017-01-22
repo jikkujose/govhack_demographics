d51900b76b0f165c9df98f3a0a0b2f6dea4838ce
Jikku Jose <jikkujose@gmail.com>
2017-01-22 18:57:30 +0530

    Expose client

--- lib/members.rb
+++ lib/members.rb
@@ -3,6 +3,8 @@ require_relative './member.rb'
 
 module DubaiHackathon
   class Members
+    attr_reader :client
+
     def initialize
       @client = Slack::Client.new token: ENV['TOKEN']
     end
